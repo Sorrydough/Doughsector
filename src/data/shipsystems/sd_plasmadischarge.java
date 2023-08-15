9745b87d1a4afd55b448781ee7afe4fef16a5165
Sorrydough <sorrydough@protonmail.com>
2023-08-15 09:08:49 -0500

    finished the plasma discharge ship system

--- src/data/shipsystems/sd_plasmadischarge.java
+++ src/data/shipsystems/sd_plasmadischarge.java
@@ -1,10 +1,13 @@
 package data.shipsystems;
 
+import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.DamagingProjectileAPI;
-import com.fs.starfarer.api.combat.MissileAPI;
 import com.fs.starfarer.api.combat.OnFireEffectPlugin;
 import com.fs.starfarer.api.combat.WeaponAPI;
+import org.lazywizard.lazylib.MathUtils;
+
+import java.awt.*;
 
 public class sd_plasmadischarge implements OnFireEffectPlugin {
 	public void onFire(DamagingProjectileAPI projectile, WeaponAPI weapon, CombatEngineAPI engine) {
@@ -13,17 +16,15 @@ public class sd_plasmadischarge implements OnFireEffectPlugin {
 		
 		float angVel = (float) (Math.signum((float) Math.random() - 0.5f) * (0.5f + Math.random()) * 720f);
 		projectile.setAngularVelocity(angVel);
-		
-		if (projectile instanceof MissileAPI) {
-			MissileAPI missile = (MissileAPI) projectile;
-			float flightTimeMult = 0.25f + 0.75f * (float) Math.random();
-			missile.setMaxFlightTime(missile.getMaxFlightTime() * flightTimeMult);
-		}
-		
-		if (weapon != null) {
-			float delay = 0.25f + 0.75f * (float) Math.random();
-			weapon.setRefireDelay(delay);
-		}
-		
+
+		float delay = 0.25f + 0.75f * (float) Math.random();
+		weapon.setRefireDelay(delay);
+
+		//loc, vel, size,
+		//brightness, duration, color
+		Global.getCombatEngine().addSmoothParticle(weapon.getLocation(), weapon.getShip().getVelocity(), MathUtils.getRandomNumberInRange(24f, 48f),
+				MathUtils.getRandomNumberInRange(0.33f, 1.33f), MathUtils.getRandomNumberInRange(0.33f, 1.33f), new Color(100,100,255,150));
+		Global.getCombatEngine().addSmoothParticle(weapon.getLocation(), weapon.getShip().getVelocity(), MathUtils.getRandomNumberInRange(24f, 48f),
+				MathUtils.getRandomNumberInRange(0.33f, 1.33f), MathUtils.getRandomNumberInRange(0.33f, 1.33f), new Color(255,100,100,150));
 	}
 }
