7dd36a5c2f6bdedce1baa51b38abc8b4c3e9bdea
Sorrydough <sorrydough@protonmail.com>
2023-08-27 21:04:00 -0500

    debugged quantum disruptor script

--- src/data/shipsystems/sd_quantumdisruptor.java
+++ src/data/shipsystems/sd_quantumdisruptor.java
@@ -11,60 +11,51 @@ import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
 import org.lazywizard.lazylib.MathUtils;
 
 public class sd_quantumdisruptor extends BaseShipSystemScript {
-	final float DISRUPTION_DUR = 1f;
-	final float DISRUPTOR_RANGE = 1000f;
-	final Color JITTER_COLOR = new Color(250, 235, 215,75);
-	final Color JITTER_UNDER_COLOR = new Color(250, 235, 215,155);
-	ShipAPI target;
-	float targetDistance;
-	float actualDisruptorRange;
-	boolean isTargetValid;
+	static final float DISRUPTION_RANGE = 1000;
 	
 	public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
 		if (Global.getCombatEngine() == null || stats.getEntity().getOwner() == -1 || stats.getVariant() == null)
 			return;
 
-		//init
+		//set jitter effects for ourselves
 		ShipAPI ship = (ShipAPI) stats.getEntity();
-		target = ship.getShipTarget();
-		if (target == null)
-			return;
-		targetDistance = MathUtils.getDistance(ship, target);
-		actualDisruptorRange = ship.getMutableStats().getSystemRangeBonus().computeEffective(DISRUPTOR_RANGE);
-		isTargetValid = !target.isFighter() && target != ship && !target.getFluxTracker().isOverloadedOrVenting() && !(targetDistance > actualDisruptorRange);
+		float jitterLevel = effectLevel;
+		if (state == State.OUT)
+			jitterLevel *= jitterLevel;
+		float jitterExtra = jitterLevel * 50;
+		ship.setJitter(this, new Color(250, 235, 215,75), jitterLevel, 4, 0f, 0 + jitterExtra);
+		ship.setJitterUnder(this, new Color(250, 235, 215,155), jitterLevel, 20, 0f, 3f + jitterExtra);
 
-		//we're going to run the script while idle because I hate alex
-		if (isTargetValid && state.ordinal() < State.COOLDOWN.ordinal()) {
-			//set jitter effects for ourself
-			float jitterLevel = effectLevel;
-			if (state == State.OUT)
-				jitterLevel *= jitterLevel;
-			float jitterRangeIncrease = jitterLevel * 50;
-			ship.setJitter(id, JITTER_COLOR, jitterLevel, 4, 0f, 0 + jitterRangeIncrease);
-			ship.setJitterUnder(id, JITTER_UNDER_COLOR, jitterLevel, 20, 0f, 3f + jitterRangeIncrease);
-
-			//overload the target, note we check effectLevel and not for active state because the system doesn't have an active duration
-			if (ship.getSystem().getEffectLevel() == 1) {
-				target.getFluxTracker().beginOverloadWithTotalBaseDuration(DISRUPTION_DUR);
-				target.getFluxTracker().playOverloadSound();
-				target.getFluxTracker().showOverloadFloatyIfNeeded("System Disruption!", JITTER_UNDER_COLOR, 4f, true);
-			}
+		//overload the target, note we check effectLevel and not for active state because the system doesn't have an active duration
+		if (ship.getSystem().getEffectLevel() == 1) {
+			ship.getShipTarget().getFluxTracker().beginOverloadWithTotalBaseDuration(1); //<<- INPUT DISRUPTION DURATION HERE
+			ship.getShipTarget().getFluxTracker().playOverloadSound();
+			ship.getShipTarget().getFluxTracker().showOverloadFloatyIfNeeded("System Disruption!", new Color(250, 235, 215,155), 4f, true);
 		}
+	}
+
+	protected static boolean isTargetValid(ShipAPI ship) {
+		ShipAPI target = ship.getShipTarget();
+		if (target == null)
+			return false;
+		float targetDistance = MathUtils.getDistance(ship, target);
+		return !target.isFighter() && target != ship && !target.getFluxTracker().isOverloadedOrVenting() &&
+				!(targetDistance > ship.getMutableStats().getSystemRangeBonus().computeEffective(DISRUPTION_RANGE));
 	}
 
 	@Override
 	public String getInfoText(ShipSystemAPI system, ShipAPI ship) {
 		if (system.isOutOfAmmo() || system.getState() != SystemState.IDLE)
-			return "RECHARGING";
-		if (targetDistance > actualDisruptorRange)
+			return null;
+		if (ship.getShipTarget() != null && MathUtils.getDistance(ship, ship.getShipTarget()) > ship.getMutableStats().getSystemRangeBonus().computeEffective(DISRUPTION_RANGE))
 			return "OUT OF RANGE";
-		if (!isTargetValid)
+		if (!isTargetValid(ship))
 			return "NO TARGET";
 		return "READY";
 	}
 
 	@Override
 	public boolean isUsable(ShipSystemAPI system, ShipAPI ship) {
-		return isTargetValid;
+		return isTargetValid(ship);
 	}
 }
