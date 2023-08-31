24c529ba89c829f93415b208ad53a6a2ba64ba42
Sorrydough <sorrydough@protonmail.com>
2023-08-31 00:30:04 -0500

    HOW DO I SET THE LIFETIME FOR A BOMB ALEX

--- src/data/shipsystems/sd_antimatterarray.java
+++ src/data/shipsystems/sd_antimatterarray.java
@@ -1,27 +1,47 @@
 package data.shipsystems;
 
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.combat.CombatEngineAPI;
-import com.fs.starfarer.api.combat.DamagingProjectileAPI;
-import com.fs.starfarer.api.combat.OnFireEffectPlugin;
-import com.fs.starfarer.api.combat.WeaponAPI;
+import com.fs.starfarer.api.combat.*;
 import org.lazywizard.lazylib.MathUtils;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("unused")
 public class sd_antimatterarray implements OnFireEffectPlugin {
+
+	final Map<ShipAPI.HullSize, Float> RANGE_MULT = new HashMap<>();
+	{
+		RANGE_MULT.put(ShipAPI.HullSize.FIGHTER, 0.95f);
+		RANGE_MULT.put(ShipAPI.HullSize.FRIGATE, 1f);
+		RANGE_MULT.put(ShipAPI.HullSize.DESTROYER, 1.05f);
+		RANGE_MULT.put(ShipAPI.HullSize.CRUISER, 1.1f);
+		RANGE_MULT.put(ShipAPI.HullSize.CAPITAL_SHIP, 1.15f);
+	}
+
 	public void onFire(DamagingProjectileAPI projectile, WeaponAPI weapon, CombatEngineAPI engine) {
-		//makes it influenced by systems expertise range mult, randomize the velocity a bit as well, so it looks nicer
-		float rangeBonus = weapon.getShip().getMutableStats().getSystemRangeBonus().computeEffective(weapon.getRange()) / weapon.getRange();
-		float speedMult = (0.25f + 0.75f * (float) Math.random()) * rangeBonus;
+		MutableShipStatsAPI stats = weapon.getShip().getMutableStats();
+		//make projectile speed depend on hull size, randomize on top
+		float speedMult = ((0.67f + 0.33f * (float)Math.random()) * RANGE_MULT.get(stats.getVariant().getHullSize()));
 		projectile.getVelocity().scale(speedMult);
+
+		//should it benefit from missile spec and EWM? not sure
+//		float damageMult = stats.getEnergyWeaponDamageMult().computeMultMod() + stats.getMissileWeaponDamageMult().computeMultMod();
+//		weapon.getDamage().setDamage(weapon.getDamage().getDamage() * damageMult);
 		
-		float angVel = (float) (Math.signum((float) Math.random() - 0.5f) * (0.5f + Math.random()) * 720f);
+		float angVel = (float)(Math.signum((float)Math.random() - 0.5f) * (0.5f + Math.random()) * 720f);
 		projectile.setAngularVelocity(angVel);
 
-		float delay = 0.25f + 0.75f * (float) Math.random();
+		float delay = 0.25f + 0.75f * (float)Math.random();
 		weapon.setRefireDelay(delay);
+
+		//makes it influenced by systems expertise range mult
+		//actually you know what fuck this bullshit what the FUCK is going on
+//		float rangeBonus = stats.getSystemRangeBonus().computeEffective(weapon.getRange()) / weapon.getRange();
+//		MissileAPI missile = (MissileAPI) projectile;
+//		float flightTimeMult = ((0.75f * rangeBonus) + 0.25f * (float)Math.random());
+//		missile.setMaxFlightTime(missile.getMaxFlightTime() * 0.1f);
 
 		//loc, vel, size,
 		//brightness, duration, color
