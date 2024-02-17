f9dc63d2aa24e31cce4f3da12dbd2cddb9dd843c
Sorrydough <sorrydough@protonmail.com>
2024-02-16 22:07:40 -0600

    some refactor

--- src/data/hullmods/sd_utilityhullmod.java
+++ src/data/hullmods/sd_utilityhullmod.java
@@ -74,17 +74,6 @@ public class sd_utilityhullmod extends BaseHullMod {
                 runOnce = false;
             }
 
-            ////////////////////////////////////////////////////////
-            //INCREDIBLY SIMPLE VENTING BEHAVIOR TO KEEP FLUX DOWN//
-            //////////////////////////////////////////////////////// TODO: IMPROVE THIS BEHAVIOR TO ONLY HAPPEN WHEN THE TARGET IS TOO FAR AWAY TO BE THREATENING
-//            if (ship.getHardFluxLevel() > 0.1 && ship.getHardFluxLevel() < 0.2 && !ship.getAIFlags().hasFlag(ShipwideAIFlags.AIFlags.HAS_INCOMING_DAMAGE) && !ship.getSystem().isActive()) {
-//                for (WeaponAPI weapon : ship.getAllWeapons()) {
-//                    if (weapon.isInBurst())
-//                        break;
-//                    ship.giveCommand(ShipCommand.VENT_FLUX, null, -1);
-//                }
-//            }
-
             ////////////////////////////////////////////////////////////
             //TELLS THE SHIP TO BACK OFF IF IT CAN'T SHOOT ITS WEAPONS//
             ////////////////////////////////////////////////////////////
@@ -105,11 +94,6 @@ public class sd_utilityhullmod extends BaseHullMod {
                     for (WeaponAPI weapon : ship.getAllWeapons())
                         if (weapon.getFluxCostToFire() + ship.getFluxTracker().getCurrFlux() > ship.getFluxTracker().getMaxFlux() * 0.85)
                             weapon.setForceNoFireOneFrame(true);
-
-
-            // FIXES SHIPS BACKING OFF WHILE VENTING EVEN THOUGH THEY'RE IN NO DANGER
-            // TODO: THIS
-
 
             ////////////////////////////
             //FIXES SUICIDING FIGHTERS// IF OUR REPLACEMENT RATE SUCKS THEN PRESERVING IT SHOULD BE OUR TOP PRIORITY
@@ -170,11 +154,6 @@ public class sd_utilityhullmod extends BaseHullMod {
                             continue;
                     }
 
-                    // we can shoot it if it's not going to use any flux
-//                    if (weapon.getFluxCostToFire() < 1 && (weapon.getAmmoPerSecond() > 1 || !weapon.usesAmmo()))
-//                        continue;
-
-
                     // if the weapon is low rof or our flux is empty, don't shoot at phased ships
                     if ((weapon.getCooldown() > 1 || weapon.usesAmmo() || ship.getFluxLevel() < 0.1) && !weapon.hasAIHint(WeaponAPI.AIHints.PD)) {
                         ShipAPI target = ship.getShipTarget();
@@ -188,117 +167,10 @@ public class sd_utilityhullmod extends BaseHullMod {
                 }
             }
 
-            //////////////////////////////////////////////////// todo: rework this to use starficz's predictedhits/futurehits function
-            //SCY'S VENTING CODE, PRACTICALLY UNCUSTOMIZED YET//
-            ////////////////////////////////////////////////////
-//            timer.advance(amount);
-//            if (timer.intervalElapsed()) {
-//                if (ship.getFluxTracker().isOverloadedOrVenting() || ship.getFluxTracker().getFluxLevel() < 0.2 || ship.getSystem().isActive())
-//                    return;
-//
-//                MissileAPI closest = AIUtils.getNearestEnemyMissile(ship);
-//                if (closest != null && MathUtils.isWithinRange(ship, closest,500))
-//                    return;
-//
-//                for (WeaponAPI wep : ship.getAllWeapons()) { // wep.isinburst doesn't work for tachyon lance
-//                    if (wep.isFiring() && (wep.isBurstBeam() || wep.isInBurst()))
-//                        return;
-//                }
-//
-////                if ( ship.getFluxTracker().getFluxLevel() < 0.5 && AIUtils.getNearbyEnemies(ship, maxRange).size() > 0) {
-////                    return;
-////                }
-//
-//                //venting need
-//                float ventingNeed;
-//                switch (ship.getHullSize()) {
-//                    case CAPITAL_SHIP:
-//                        ventingNeed = 2*(float) Math.pow(ship.getFluxTracker().getFluxLevel(),5f);
-//                        break;
-//                    case CRUISER:
-//                        ventingNeed = 1.5f*(float) Math.pow(ship.getFluxTracker().getFluxLevel(),4f);
-//                        break;
-//                    case DESTROYER:
-//                        ventingNeed = (float) Math.pow(ship.getFluxTracker().getFluxLevel(),3f);
-//                        break;
-//                    default:
-//                        ventingNeed = (float) Math.pow(ship.getFluxTracker().getFluxLevel(),2f);
-//                        break;
-//                }
-//
-//                float hullFactor;
-//                switch (ship.getHullSize()) {
-//                    case CAPITAL_SHIP:
-//                        hullFactor=(float) Math.pow(ship.getHullLevel(),0.4f);
-//                        break;
-//                    case CRUISER:
-//                        hullFactor=(float) Math.pow(ship.getHullLevel(),0.6f);
-//                        break;
-//                    case DESTROYER:
-//                        hullFactor=ship.getHullLevel();
-//                        break;
-//                    default:
-//                        hullFactor=(float) Math.pow(ship.getHullLevel(),2f);
-//                        break;
-//                }
-//
-//                //situational danger
-//                float dangerFactor = 0;
-//                List<ShipAPI> nearbyEnemies = AIUtils.getNearbyEnemies(ship, 2000f);
-//                for (ShipAPI enemy : nearbyEnemies) {
-//                    //reset often with timid or cautious personalities
-//                    FleetSide side = FleetSide.PLAYER;
-//                    if (ship.getOriginalOwner() > 0){
-//                        side = FleetSide.ENEMY;
-//                    }
-//                    if(Global.getCombatEngine().getFleetManager(side).getDeployedFleetMember(ship)!=null){
-//                        PersonalityAPI personality = (Global.getCombatEngine().getFleetManager(side).getDeployedFleetMember(ship)).getMember().getCaptain().getPersonalityAPI();
-//                        if(personality.getId().equals("timid") || personality.getId().equals("cautious")){
-//                            if (enemy.getFluxTracker().isOverloaded() && enemy.getFluxTracker().getOverloadTimeRemaining() > ship.getFluxTracker().getTimeToVent()) {
-//                                continue;
-//                            }
-//                            if (enemy.getFluxTracker().isVenting() && enemy.getFluxTracker().getTimeToVent() > ship.getFluxTracker().getTimeToVent()) {
-//                                continue;
-//                            }
-//                        }
-//                    }
-//
-//                    switch (enemy.getHullSize()) {
-//                        case CAPITAL_SHIP:
-//                            dangerFactor+= Math.max(0, 3f -(MathUtils.getDistanceSquared(enemy.getLocation(), ship.getLocation())/1000000));
-//                            break;
-//                        case CRUISER:
-//                            dangerFactor+= Math.max(0, 2.25f -(MathUtils.getDistanceSquared(enemy.getLocation(), ship.getLocation())/1000000));
-//                            break;
-//                        case DESTROYER:
-//                            dangerFactor+= Math.max(0, 1.5f -(MathUtils.getDistanceSquared(enemy.getLocation(), ship.getLocation())/1000000));
-//                            break;
-//                        case FRIGATE:
-//                            dangerFactor+= Math.max(0, 1f -(MathUtils.getDistanceSquared(enemy.getLocation(), ship.getLocation())/1000000));
-//                            break;
-//                        default:
-//                            dangerFactor+= Math.max(0, 0.5f -(MathUtils.getDistanceSquared(enemy.getLocation(), ship.getLocation())/640000));
-//                            break;
-//                    }
-//                }
-//
-//                float decisionLevel = (ventingNeed * hullFactor + 1) / (dangerFactor + 1);
-//
-//                if (decisionLevel >= 1.5f || (ship.getFluxTracker().getFluxLevel() > 0.1f && dangerFactor == 0)) {
-//                    ship.giveCommand(ShipCommand.VENT_FLUX, null, 0);
-//                }
-//            }
-
-            ////////////////////////////
-            //STARFICZ' SHIELD AI CODE// todo: rework all this shit to be better for my epic agile ships
-            ////////////////////////////
-//            if (shield != null) {
-//                if (shield.isOn() ^ wantToShield(amount))
-//                    ship.giveCommand(ShipCommand.TOGGLE_SHIELD_OR_PHASE_CLOAK, null, 0);
-//                else
-//                    ship.blockCommandForOneFrame(ShipCommand.TOGGLE_SHIELD_OR_PHASE_CLOAK);
-//            }
-
+            ////////////////////////////////////////////////////////
+            // CUSTOM VENT AND ARMORTANKING CODE BEYOND THIS PART //
+            // USES STARFICZ'S AI UTILS ////////////////////////////
+            ////////////////////////////////////////////////////////
 
             incomingHitsTracker.advance(amount);
             if (incomingHitsTracker.intervalElapsed()) {
@@ -313,15 +185,15 @@ public class sd_utilityhullmod extends BaseHullMod {
                 }
             }
 
-            if (isArmorDamageAcceptable(amount, potentialHitsForVenting)) {
-                boolean isWeaponFiring = false;
-                for (WeaponAPI weapon : ship.getAllWeapons()) { // wep.isinburst doesn't work for tachyon lance
-                    if (weapon.isFiring() && (weapon.isBurstBeam() || weapon.isInBurst())) {
-                        isWeaponFiring = true;
-                        break;
-                    }
+            boolean isWeaponFiring = false;
+            for (WeaponAPI weapon : ship.getAllWeapons()) { // wep.isinburst doesn't work for tachyon lance
+                if (weapon.isFiring() && (weapon.isBurstBeam() || weapon.isInBurst())) {
+                    isWeaponFiring = true;
+                    break;
                 }
-                if (ship.getFluxLevel() > 0.2 && !isWeaponFiring)
+            }
+            if (!isWeaponFiring && isArmorDamageAcceptable(amount, potentialHitsForVenting)) {
+                if (ship.getFluxLevel() > 0.2)
                     ship.giveCommand(ShipCommand.VENT_FLUX, null, 0);
             }
 
@@ -334,14 +206,12 @@ public class sd_utilityhullmod extends BaseHullMod {
                     ship.blockCommandForOneFrame(ShipCommand.TOGGLE_SHIELD_OR_PHASE_CLOAK);
             }
         }
-
         private final IntervalUtil incomingHitsTracker = new IntervalUtil(0.05f, 0.05f);
         public float lastUpdatedTime = 0;
         public float lastShieldOnTime = 0;
         public List<FutureHit> potentialHitsForVenting = new ArrayList<>();
         public List<FutureHit> potentialHitsForShield = new ArrayList<>();
         public List<FutureHit> potentialHits = new ArrayList<>();
-
         public boolean isArmorDamageAcceptable(float amount, List<FutureHit> potentialHits) {
             // calculate how much damage the ship would take if shields went down
             float currentTime = Global.getCombatEngine().getTotalElapsedTime(false);
@@ -380,122 +250,4 @@ public class sd_utilityhullmod extends BaseHullMod {
             return isArmorDamageAcceptable;
         }
     }
-
-
-
-
-
-
-
-//        private final IntervalUtil tracker = new IntervalUtil(0.2f, 0.3f); //Seconds
-//        public List<Float> omniShieldDirections = new ArrayList<>();
-//        public boolean wantToShield(float amount, List<FutureHit> potentialHits) {
-//            tracker.advance(amount);
-//            if (tracker.intervalElapsed()) {
-//                float BUFFER_ARC = 10f;
-//                // prefilter expensive one time functions
-//                if (shield.getType() == ShieldAPI.ShieldType.FRONT) {
-//                    List<FutureHit> filteredHits = new ArrayList<>();
-//                    for (FutureHit hit : potentialHits) {
-//                        if (Misc.isInArc(ship.getFacing(), shield.getArc() + BUFFER_ARC, hit.angle)) {
-//                            filteredHits.add(hit);
-//                        }
-//                    }
-//                    potentialHits = filteredHits;
-//                } else {
-//                    List<Float> candidateShieldDirections = new ArrayList<>();
-//                    float FUZZY_RANGE = 1f;
-//                    for (FutureHit hit : potentialHits) {
-//                        boolean tooClose = false;
-//                        for (float candidateDirection : candidateShieldDirections) {
-//                            if (Math.abs(hit.angle - candidateDirection) < FUZZY_RANGE) {
-//                                tooClose = true;
-//                                break;
-//                            }
-//                        }
-//                        if (!tooClose)
-//                            candidateShieldDirections.add(hit.angle);
-//                    }
-//                    if (candidateShieldDirections.isEmpty()) candidateShieldDirections.add(ship.getFacing());
-//                    omniShieldDirections = candidateShieldDirections;
-//                }
-//            }
-//
-//            // calculate how much damage the ship would take if shields went down
-//            float currentTime = Global.getCombatEngine().getTotalElapsedTime(false);
-//            float timeElapsed = currentTime - lastUpdatedTime;
-//            float armor = getWeakestTotalArmor(ship);
-//
-//            float unfoldTime = shield.getUnfoldTime() * 2; // double it to be safe
-//            float bufferTime = unfoldTime / 3; //TODO: get real shield activate delay, unfoldTime / 4 is: "looks about right"
-//            if (shield.isOn())
-//                lastShieldOnTime = currentTime;
-//            float delayTime = Math.max(0.5f - (currentTime - lastShieldOnTime), 0f); //TODO: get real shield deactivate time
-//
-//            float shieldHardFluxSavedIfNoShield = 0f;
-//            float armorDamageIfNoShield = Float.POSITIVE_INFINITY;
-//            float hullDamageIfNoShield = Float.POSITIVE_INFINITY;
-//            float empDamageIfNoShield = 0f;
-//
-//            List<Float> shieldDirections = new ArrayList<>();
-//            if (shield.getType() == ShieldAPI.ShieldType.FRONT) shieldDirections.add(ship.getFacing());
-//            else shieldDirections.addAll(omniShieldDirections);
-//
-//            for (float shieldDirection : shieldDirections) {
-//                float currentHullDamage = 0f;
-//                float currentArmor = armor;
-//                float currentShieldHardFluxSaved = 0f;
-//                float currentEMPDamage = 0f;
-//                float currentBufferTime = Float.POSITIVE_INFINITY;
-//                float bestBufferTime = 0f;
-//                for (sd_util.FutureHit hit : potentialHits) {
-//                    float offAngle = Math.abs(MathUtils.getShortestRotation(shieldDirection, hit.angle));
-//                    float timeToBlock = (offAngle / (shield.getArc() / 2)) * unfoldTime + delayTime;
-//                    float timeToHit = (hit.timeToHit - timeElapsed);
-//                    if (timeToHit < -0.1f) continue; // skip hits that have already happened
-//                    if (timeToHit < (timeToBlock + bufferTime)) {
-//                        if (!hit.softFlux) currentShieldHardFluxSaved += fluxToShield(hit.damageType, hit.damage, ship);
-//                        Pair<Float, Float> trueDamage = damageAfterArmor(hit.damageType, hit.damage, hit.hitStrength, currentArmor, ship);
-//                        currentArmor = Math.max(currentArmor - trueDamage.one, 0f);
-//                        currentHullDamage += trueDamage.two;
-//                        currentEMPDamage += hit.empDamage;
-//                    } else {
-//                        currentBufferTime = Math.min(timeToHit - timeToBlock, currentBufferTime);
-//                    }
-//                }
-//
-//                boolean betterDirectionFound = false;
-//                if ((armor - currentArmor) < armorDamageIfNoShield) betterDirectionFound = true;
-//                else if ((armor - currentArmor) == armorDamageIfNoShield) {
-//                    if (currentHullDamage < hullDamageIfNoShield) betterDirectionFound = true;
-//                    if (currentHullDamage == hullDamageIfNoShield && currentBufferTime > bestBufferTime) betterDirectionFound = true;
-//                }
-//
-//                if (betterDirectionFound) {
-//                    shieldHardFluxSavedIfNoShield = currentShieldHardFluxSaved;
-//                    armorDamageIfNoShield = (armor - currentArmor);
-//                    hullDamageIfNoShield = currentHullDamage;
-//                    empDamageIfNoShield = currentEMPDamage;
-//                }
-//            }
-//
-//            float lowDamage = 0.001f; // 0.1% hull damage
-//            float highDamage = 0.020f; // 2.0% hull damage
-//
-//            // shield based on the current flux level
-//            boolean wantToShield = (hullDamageIfNoShield + armorDamageIfNoShield + empDamageIfNoShield / 3) > ship.getHitpoints() * (ship.getFluxLevel() * highDamage + (1 - ship.getFluxLevel()) * lowDamage);
-//
-//            // if the damage is high enough to shield, but flux is high, armor tank KE
-//            if (wantToShield && shieldHardFluxSavedIfNoShield/(hullDamageIfNoShield + armorDamageIfNoShield)  > (ship.getFluxLevel() * 2f + (1 - ship.getFluxLevel()) * 7f))
-//                wantToShield = false;
-//
-//            if ((shieldHardFluxSavedIfNoShield + ship.getCurrFlux()) / ship.getMaxFlux() > 1) // Prevent overloads...
-//                wantToShield = false;
-//
-//            if ((hullDamageIfNoShield + armorDamageIfNoShield) > 4000f) // Unless a reaper is on the way, in that case try and block it no matter what
-//                wantToShield = true;
-//
-//            return wantToShield;
-//        }
-//    }
 }
